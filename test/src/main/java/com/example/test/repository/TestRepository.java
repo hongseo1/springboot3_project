e3fe7dbffb8702f9dc4fd526a3571ae77cd813ea
hongseo1 <hongseow109@gmil.com>
2024-04-29 10:29:48 +0900

    240429

--- test/src/main/java/com/example/test/repository/TestRepository.java
+++ test/src/main/java/com/example/test/repository/TestRepository.java
@@ -1,8 +1,10 @@
 package com.example.test.repository;
 
 import com.example.test.entity.Test;
+import org.springframework.data.jdbc.repository.query.Query;
 import org.springframework.data.repository.CrudRepository;
 
 public interface TestRepository extends CrudRepository<Test, Integer> {
-
+    @Query("SELECT id FROM test ORDER BY RAND() limit 1") //jdbc안에 있는 쿼리문(실제 데이터베이스 언어) 실행해줌
+    Integer getRandomId();
 }
