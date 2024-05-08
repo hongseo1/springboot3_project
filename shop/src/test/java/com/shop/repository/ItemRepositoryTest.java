bf786d219856c2ff9b7018cd085f95382b684b6d
hongseo1 <hongseow109@gmil.com>
2024-05-08 12:57:07 +0900

    240508

--- shop/src/test/java/com/shop/repository/ItemRepositoryTest.java
+++ shop/src/test/java/com/shop/repository/ItemRepositoryTest.java
@@ -1,7 +1,12 @@
 package com.shop.repository;
 
+import com.querydsl.jpa.impl.JPAQuery;
+import com.querydsl.jpa.impl.JPAQueryFactory;
 import com.shop.constant.ItemSellStatus;
 import com.shop.entity.Item;
+import com.shop.entity.QItem;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -18,6 +23,10 @@ import static org.junit.jupiter.api.Assertions.*;
 class ItemRepositoryTest {
     @Autowired
     ItemRepository itemRepository;
+
+    @PersistenceContext
+    EntityManager em;
+
     @Test
     @DisplayName("상품 저장 테스트")
     public void createItemTest(){
@@ -103,6 +112,22 @@ class ItemRepositoryTest {
         this.createItemList();
         List<Item> itemList = itemRepository.findByItemDetailByNative("테스트 상품 상세 설명");
         for (Item item : itemList){
+            System.out.println(item.toString());
+        }
+    }
+
+    @Test
+    @DisplayName("Querydsl 조회 테스트")
+    public void queryDslTest(){
+        this.createItemList();
+        JPAQueryFactory queryFactory = new JPAQueryFactory(em);
+        QItem qItem = QItem.item;
+        JPAQuery<Item> query = queryFactory.selectFrom(qItem)
+            .where(qItem.itemSellStatus.eq(ItemSellStatus.SELL))
+            .where(qItem.itemDetail.like("%"+"테스트 상품 상세 설명"+"%"))
+            .orderBy(qItem.price.desc());
+        List<Item> itemList = query.fetch();
+        for(Item item : itemList){
             System.out.println(item.toString());
         }
     }
